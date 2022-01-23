ee243cd6c7352c62c33b895f56eeb49c85700b42
virupabandi <89890365+virupabandi@users.noreply.github.com>
2022-01-23 18:59:23 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is automatic CI-testing3" );
+        System.out.println( "This is automatic CI-testing4" );
     }
 }
